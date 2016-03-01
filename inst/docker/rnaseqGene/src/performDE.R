e8c719d9dbaeb5655d3299d530475157ba00fe04
t.yin <t.yin@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-01 04:18:27 +0000

    update md for rnaseqgene
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/sevenbridges@114128 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/docker/rnaseqGene/src/performDE.R
+++ inst/docker/rnaseqGene/src/performDE.R
@@ -1,5 +1,5 @@
 #!/usr/bin/Rscript
-"usage: performDE.R (--design=<file> --gtffile=<file> --bamfiles=<file>)
+"usage: performDE.R [--design=<file> --gtffile=<file> --bamfiles=<file>]
 
 options:
 --bamfiles=<file> bamfiles
@@ -10,12 +10,31 @@ options:
 library(docopt)
 opts <- docopt(doc)
 
+.design <- opts$design
+if(is.null(.design))
+    .design <- system.file("extdata/sample_table.csv", package="airway")
+
+.bamfiles <- opts$bamfiles
+if(is.null(.bamfiles)){
+    .bamfiles <- list.files(system.file("extdata", package="airway"), "*.bam",
+                            full.names = TRUE)
+}else{
+   .bamfiles <- strsplit(opts$bamfiles, ",")[[1]]
+}
+
+.gtffile <- opts$gtffile
+if(is.null(.gtffile))
+    .gtffile <- system.file("extdata/Homo_sapiens.GRCh37.75_subset.gtf", 
+                            package="airway")
 ## create param list
-lst <- list(design = normalizePath(opts$design), 
-            gtffile = normalizePath(opts$gtffile), 
-            bamfiles = normalizePath(strsplit(opts$bamfiles, ",")[[1]]))
+lst <- list(design = normalizePath(.design), 
+            gtffile = normalizePath(.gtffile), 
+            bamfiles = normalizePath(.bamfiles))
 
 ## execute your Rmarkdown with these parameters
-rmarkdown::render("/report/rnaseqGene.Rmd", BiocStyle::html_document(toc = TRUE),
-                  output_dir = ".", params = lst)
+## rmarkdown::render("/report/rnaseqGene.Rmd", BiocStyle::pdf_document(toc = TRUE),
+##                  output_dir = ".", params = lst)
 
+rmarkdown::render("/report/rnaseqGene.Rmd", "pdf_document", 
+                  output_dir = ".",
+                  params = lst)
