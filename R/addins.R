8d313d4239f35d5871c0badad9959e0943ad88db
t.yin <t.yin@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-07 22:36:39 +0000

    add addins (unfinished) and update file api that accept metadata filter and origin.task
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/sevenbridges@114329 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/addins.R
@@ -0,0 +1,26 @@
+ToolUI <- function(){
+    # Get the document context.
+    context <- rstudioapi::getActiveDocumentContext()
+    
+    # Set the default data to use based on the selection.
+    text <- context$selection[[1]]$text
+    defaultData <- text
+    
+    # Generate UI for the gadget.
+    ui <- miniPage(
+        gadgetTitleBar("Describe your tool"),
+        miniCotentPanel(
+            stableColumnLayout(
+                textInput("data", "Data", value = defaultData),
+                textInput("subset", "Subset Expression")
+            ),
+            uiOutput("pending")
+        )
+    )
+    
+    
+    # Server code for the gadget.
+    server <- function(input, output, session) {
+        
+    }
+}
