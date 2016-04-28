f061d96e3b2b799bd1d07bc7942d900d281e36a8
Tengfei Yin <yintengfei@gmail.com>
2016-04-28 19:48:42 -0400

    this close #8, I add spin examples

---
+++ inst/docker/sevenbridges/src/runif2spin.R
@@ -0,0 +1,24 @@
+#'---
+#'title: "Uniform randome number generator example"
+#'output:
+#'    html_document:
+#'    toc: true
+#'number_sections: true
+#'highlight: haddock
+#'---
+    
+#'## summary report
+#'
+#'This is a randome number generator
+
+#+
+args <- commandArgs(TRUE)
+
+r <- runif(n = as.integer(args[1]),
+           min = as.numeric(args[2]),
+           max = as.numeric(args[3]))
+head(r)
+summary(r)
+hist(r)
+
+
