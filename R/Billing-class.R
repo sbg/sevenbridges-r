f2ec418bfd0222b9c12128d0c0a87dc98ff101de
Nan Xiao <road2stat@gmail.com>
2016-11-04 20:57:23 -0400

    reformat core and billing

--- R/Billing-class.R
+++ R/Billing-class.R
@@ -1,81 +1,87 @@
-## TODO: when it's get stable, define billing object
-## PrivilegesEnum <- setSingleEnum("Privileges", c(""))
-## Billing
-## to make it simple to update, return a list, not an object, because no action defined an this object
+# TODO: when it got stable, define billing object
+# PrivilegesEnum <- setSingleEnum("Privileges", c(""))
+# Billing
+# to make it simple to update, return a list, not an object,
+# because no action defined an this object
 Billing <- setRefClass("Billing", contains = "Item",
-                       fields = list(id = "characterORNULL",
-                           name = "characterORNULL",
-                           owner = "characterORNULL",
-                           privileges = "listORNULL",
-                           type = "characterORNULL",
-                           pending = "logicalORNULL",
-                           disabled = "logicalORNULL",
-                           active = "logicalORNULL",
-                           balance = "listORNULL",
-                           project_breakdown = "listORNULL",
-                           total_spending = "listORNULL"), ## 1.1
-                       
+                       fields = list(id         = "characterORNULL",
+                                     name       = "characterORNULL",
+                                     owner      = "characterORNULL",
+                                     privileges = "listORNULL",
+                                     type       = "characterORNULL",
+                                     pending    = "logicalORNULL",
+                                     disabled   = "logicalORNULL",
+                                     active     = "logicalORNULL",
+                                     balance    = "listORNULL",
+                                     project_breakdown = "listORNULL",
+                                     total_spending    = "listORNULL"),  # 1.1
+
                        methods = list(
-                           
-                           initialize = function(id = NULL,
-                               name = NULL,
-                               owner = NULL, privileges = list(),
-                               type = NULL, pending = NULL,
-                               disabled = NULL, active = NULL, balance = list(),
-                               project_breakdown = list(),
-                               total_spending = list(), ...){
 
-                               id <<- id
-                               name <<- name
-                               owner <<- owner
-                               privileges <<- privileges
-                               type <<- type
-                               disabled <<- disabled
-                               active <<- active
-                               balance <<- balance
+                         initialize = function(id    = NULL,
+                                               name  = NULL,
+                                               owner = NULL,
+                                               privileges = list(),
+                                               type  = NULL,
+                                               pending  = NULL,
+                                               disabled = NULL,
+                                               active   = NULL,
+                                               balance  = list(),
+                                               project_breakdown = list(),
+                                               total_spending = list(), ...) {
 
-                               ## for breakdown
-                               project_breakdown <<- project_breakdown
-                               total_spending <<- total_spending
+                           id <<- id
+                           name <<- name
+                           owner <<- owner
+                           privileges <<- privileges
+                           type <<- type
+                           disabled <<- disabled
+                           active <<- active
+                           balance <<- balance
 
-                               callSuper(...)
-                               
-                           },
-                           
-                           show = function(){
-                               .showFields(.self, "== Billing ==",
-                                           values = c("id", "href", "name",
-                                               "owner", "privileges", "type",
-                                               "disabled", "active", "balance",
-                                               "project_breakdown",
-                                               "total_spending"))
+                           # for breakdown
+                           project_breakdown <<- project_breakdown
+                           total_spending <<- total_spending
 
-                           }
+                           callSuper(...)
+
+                         },
+
+                         show = function() {
+                           .showFields(.self, "== Billing ==",
+                                       values = c("id", "href", "name",
+                                                  "owner", "privileges", "type",
+                                                  "disabled", "active", "balance",
+                                                  "project_breakdown", "total_spending"))
+                         }
+
                        ))
 
-.asBilling <- function(x){
-    Billing(id = x$id,
-            href = x$href,
-            name = x$name,
-            owner = x$owner,
-            privileges = x$privileges,
-            type = x$type,
-            disabled = x$disabled,
-            active = x$active,
-            balance = x$balance,
-            response = response(x),
-            ## for breakdown
-            project_breakdown = x$project_breakdown,
-            total_spending = x$total_spending)
+.asBilling <- function(x) {
+
+  Billing(id    = x$id,
+          href  = x$href,
+          name  = x$name,
+          owner = x$owner,
+          privileges = x$privileges,
+          type  = x$type,
+          disabled   = x$disabled,
+          active     = x$active,
+          balance    = x$balance,
+          response   = response(x),
+          # for breakdown
+          project_breakdown = x$project_breakdown,
+          total_spending    = x$total_spending)
+
 }
 
 BillingList <- setListClass("Billing", contains = "Item0")
 
-.asBillingList <- function(x){
-    obj <- BillingList(lapply(x$items, .asBilling))
-    obj@href <- x$href
-    obj@response <- response(x)
-    obj
+.asBillingList <- function(x) {
+
+  obj <- BillingList(lapply(x$items, .asBilling))
+  obj@href <- x$href
+  obj@response <- response(x)
+  obj
+
 }
-
-
--- R/core.R
+++ R/core.R
@@ -1,6 +1,6 @@
-#' wrapper of http logic for SBG API
+#' Wrapper of http logic for SBG API
 #'
-#' wrapper of http logic for SBG API
+#' Wrapper of http logic for SBG API
 #'
 #' Used for advanced users and the core method for higher level API in
 #' this package, please refer to the easy api manual and the two
@@ -9,20 +9,20 @@
 #' @param token authenticate token string.
 #' @param version API version number, default 1.1.
 #' @param path path connected with base_url.
-#' @param method one of 'GET', 'POST', 'PUT', 'Delete'
+#' @param method one of \code{"GET"}, \code{"POST"}, \code{"PUT"}, \code{"Delete"}.
 #' @param query Passed to httr package GET/POST call.
 #' @param body Passed to httr package GET/POST/PUT/DELETE call.
 #' @param encode If the body is a named list, how should it be
-#' encoded? Default here is "json". Can be one of form (application/x-www-form-urlencoded),
-#' multipart, (multipart/form-data), or json (application/json).  For
-#' "multipart", list elements can be strings or objects created by
-#' upload_file. For "form", elements are coerced to strings and escaped,
-#' use I() to prevent double-escaping. For "json", parameters are
-#' automatically "unboxed" (i.e. length 1 vectors are converted to
-#' scalars). To preserve a length 1 vector as a vector, wrap in I().
-#' @param limit how many results to return 
-#' @param offset the point at which to start displaying them
-#' @param base_url defeault is 'https://api.sbgenomics.com/1.1'
+#' encoded? Default here is "json". Can be one of form
+#' (application/x-www-form-urlencoded), multipart, (multipart/form-data),
+#' or json (application/json). For "multipart", list elements can be strings
+#' or objects created by upload_file. For "form", elements are coerced to
+#' strings and escaped, use I() to prevent double-escaping. For "json",
+#' parameters are automatically "unboxed" (i.e. length 1 vectors are converted
+#' to scalars). To preserve a length 1 vector as a vector, wrap in I().
+#' @param limit How many results to return
+#' @param offset The point at which to start displaying them
+#' @param base_url defeault is "https://api.sbgenomics.com/1.1"
 #' @param ... passed to GET/PUT/DELETE/PATCH/POST call.
 #'
 #' @return returned request list of httr
@@ -38,11 +38,12 @@
 #' api(token = token, path = 'project', method = "GET")
 #' }
 api = function (token = NULL, version = '1.1', path = NULL,
-    method = c('GET', 'POST', 'PUT', 'DELETE', 'PATCH'),
-    query = NULL, body = list(), encode = "json", 
-    limit = getOption("sevenbridges")$limit,
-    offset = getOption("sevenbridges")$offset, 
-    base_url = paste0("https://api.sbgenomics.com/", version, "/"), ...) {
+                method = c('GET', 'POST', 'PUT', 'DELETE', 'PATCH'),
+                query = NULL, body = list(), encode = "json",
+                limit = getOption("sevenbridges")$limit,
+                offset = getOption("sevenbridges")$offset,
+                base_url = paste0("https://api.sbgenomics.com/", version, "/"),
+                ...) {
 
     if (is.null(token))
         stop('token must be provided')
@@ -51,35 +52,34 @@ api = function (token = NULL, version = '1.1', path = NULL,
 
     headers = c(
         'X-SBG-Auth-Token' = token
-        ## 'Accept' = 'application/json',
-        ## 'Content-type' = 'application/json'
+        # 'Accept' = 'application/json',
+        # 'Content-type' = 'application/json'
     )
 
-    ## setup query
-    query <- c(query, list(limit = limit, offset = offset))
-    idx <- !sapply(query, is.null)
-    if(any(idx)){
+    # setup query
+    query = c(query, list(limit = limit, offset = offset))
+    idx = !sapply(query, is.null)
+    if (any(idx)) {
         query <- query[idx]
-    }else{
+    } else {
         query <- NULL
     }
-
 
     switch(method,
            GET = {
                GET2(paste0(base_url, path),
-                   add_headers(.headers = headers), query = query, ...)
+                    add_headers(.headers = headers), query = query, ...)
            },
            POST = {
-               ## stopifnot(is.list(body))
-               ## body_json = toJSON(body, auto_unbox = TRUE)
+               # stopifnot(is.list(body))
+               # body_json = toJSON(body, auto_unbox = TRUE)
                POST2(paste0(base_url, path),
-                    add_headers(.headers = headers), query = query,
-                    body = body, encode = encode,  ...)
+                     add_headers(.headers = headers), query = query,
+                     body = body, encode = encode,  ...)
            },
            PUT = {
-               ## stopifnot(is.list(body))
-               ## body_json = toJSON(body, auto_unbox = TRUE)
+               # stopifnot(is.list(body))
+               # body_json = toJSON(body, auto_unbox = TRUE)
                PUT(paste0(base_url, path),
                    add_headers(.headers = headers),
                    body = body, encode = encode, ...)
@@ -89,160 +89,162 @@ api = function (token = NULL, version = '1.1', path = NULL,
                       add_headers(.headers = headers), ...)
            },
            PATCH = {
-               ## stopifnot(is.list(body))
-               ## body_json = toJSON(body, auto_unbox = TRUE)
+               # stopifnot(is.list(body))
+               # body_json = toJSON(body, auto_unbox = TRUE)
                PATCH(paste0(base_url, path),
                      add_headers(.headers = headers),
                      body = body,
                      encode = encode, ...)
            })
-    
-    
+
 }
 
-
-#' check request status
+#' Check request status
 #'
-#' check request status
+#' Check request status
 #'
 #' @return request content or the message
 #'
 #' @keywords internal
-status_check = function (req, as = "parsed", ...) {
+status_check = function (req, as = 'parsed', ...) {
+
     if (status_code(req) %in% c('200', '201', '202', '204')) {
+
         res <- content(req, as = as, ...)
         if(!is.null(res))
-            attr(res, "response") <- req
+            attr(res, 'response') = req
         return(res)
+
     } else if (status_code(req) %in% c('401', '403', '404', '503')) {
 
-        msg = content(req, as = as, ...)$message       
-        stop(paste0('HTTP Status ', status_code(req), ': ', msg), call. = FALSE)            
-        
+        msg = content(req, as = as, ...)$message
+        stop(paste0('HTTP Status ', status_code(req), ': ', msg), call. = FALSE)
 
     } else {
 
-        if("message" %in% names(content(req, as = as, ...))){
+        if ("message" %in% names(content(req, as = as, ...))) {
+
             msg = content(req, as = as, ...)$message
-        }else{
+
+        } else {
+
             msg = NULL
+
         }
 
-        if(is.null(msg)){
-            if(status_code(req) %in% names(.codes)){
-                msg <- .codes[[status_code(req)]]                
+        if (is.null(msg)) {
+            if (status_code(req) %in% names(.codes)) {
+                msg <- .codes[[status_code(req)]]
             }
-            if(is.null(msg)){
+            if (is.null(msg)) {
                 print(content(req, as = as, ...))
                 stop(paste('Error of unknown type occured', status_code(req)))
-            }else{
-                stop(paste0('HTTP Status ', status_code(req), ': ', msg), call. = FALSE)       
+            } else {
+                stop(paste0('HTTP Status ', status_code(req), ': ', msg), call. = FALSE)
             }
-        }else{
-            stop(paste0('HTTP Status ', status_code(req), ': ', msg), call. = FALSE)           
+        } else {
+            stop(paste0('HTTP Status ', status_code(req), ': ', msg), call. = FALSE)
         }
-        
+
     }
 
 }
 
-
-
-## well this code is from specification v2
+# This code is from specification v2
 .codes = list(
-    ## 0xxx: Platform maintenance errors
+
+    # 0xxx: Platform maintenance errors
     "0" = "Seven Bridges Genomics platform is currently under maintenance.",
 
-    ## 1xxx: General errors
-    "1000"= "Allowed Rate limit exceeded! Check response headers.",
+    # 1xxx: General errors
+    "1000" = "Allowed Rate limit exceeded! Check response headers.",
 
-    ## 2xxx: Users
-    "2000" = "User service is currently unavailable", 
-    "2001" = "Not enough privileges to access requested user info.", 
-    "2002" = "Requested user does not exist.", 
-    "2003" = "Requested user already exists.", 
+    # 2xxx: Users
+    "2000" = "User service is currently unavailable",
+    "2001" = "Not enough privileges to access requested user info.",
+    "2002" = "Requested user does not exist.",
+    "2003" = "Requested user already exists.",
 
-    ## "3xxx" = "Projects
-    "3000" = "Project service is currently unavailable", 
-    "3001" = "Not enough privileges to access requested project/member", 
-    "3002" = "Requested project or member does not exist", 
-    "3003" = "Requested project/member already exists", 
-    "3004" = "Owner's username must not be null or empty string", 
-    "3005" = "Member username must not be null or empty string", 
-    "3006" = "Project id must not be null or an empty string", 
-    "3007" = "Project name must not be null or empty string", 
-    "3008" = "Billing group id must not be null or empty string", 
+    # 3xxx: Projects
+    "3000" = "Project service is currently unavailable",
+    "3001" = "Not enough privileges to access requested project/member",
+    "3002" = "Requested project or member does not exist",
+    "3003" = "Requested project/member already exists",
+    "3004" = "Owner's username must not be null or empty string",
+    "3005" = "Member username must not be null or empty string",
+    "3006" = "Project id must not be null or an empty string",
+    "3007" = "Project name must not be null or empty string",
+    "3008" = "Billing group id must not be null or empty string",
     "3009" = "Project type must not be null or empty string",
-    "3010" = "Project type can be either CWL for developer projects or LEGACY for old style projects", 
+    "3010" = "Project type can be either CWL for developer projects or LEGACY for old style projects",
     "3011" = "Project permissions must not null or an empty value",
-    "3012" = "Malformed project id. Expecting owner/project", 
+    "3012" = "Malformed project id. Expecting owner/project",
 
-    ## 4xxx" = "Billing"
-    "4000" = "Billing service is currently unavailable", 
-    "4001" = "Insufficient privileges to access the requested billing group/invoice", 
-    "4002" = "Requested billing group/invoice does not exist", 
-    "4003" = "Requested billing group/invoice already exist", 
-    "4004" = "Billing group id must not be null or an empty string", 
-    "4005" = "Billing group id must be a valid UUID", 
-    "4006" = "You are not a member of this billing group", 
-    "4007" = "Invoice id must not be null or an empty string", 
+    # 4xxx: Billing
+    "4000" = "Billing service is currently unavailable",
+    "4001" = "Insufficient privileges to access the requested billing group/invoice",
+    "4002" = "Requested billing group/invoice does not exist",
+    "4003" = "Requested billing group/invoice already exist",
+    "4004" = "Billing group id must not be null or an empty string",
+    "4005" = "Billing group id must be a valid UUID",
+    "4006" = "You are not a member of this billing group",
+    "4007" = "Invoice id must not be null or an empty string",
 
-    ## "5xxx" = "Files
-    "5000" = "File service is currently unavailable", 
-    "5001" = "Insufficient privileges to access the requested file", 
+    # 5xxx: Files
+    "5000" = "File service is currently unavailable",
+    "5001" = "Insufficient privileges to access the requested file",
     "5002" = "Requested file does not exist",
-    "5004" = "Requested file already exists", 
-    "5005" = "Malformed project query parameter. Expecting ?project=owner/project", 
+    "5004" = "Requested file already exists",
+    "5005" = "Malformed project query parameter. Expecting ?project=owner/project",
     "5006" = "Metadata validation failed" ,
-    "5007" = "File copy failed", 
-    "5008" = "File renaming not allowed", 
+    "5007" = "File copy failed",
+    "5008" = "File renaming not allowed",
 
-    ## "6xxx" = "Apps"
-    "6000" = "App service is currently unavailable", 
-    "6001" = "Insufficient privileges to access the requested app/revision", 
-    "6002" = "Requested app/revision does not exist", 
-    "6003" = "Requested app/revision already exists", 
-    "6004" = "App name must not be null or an empty string", 
-    "6006" = "Project owner must not be null or an empty string", 
-    "6007" = "Project must not be null or an empty string", 
-    "6008" = "App revision must not be null or an empty string", 
+    # 6xxx: Apps
+    "6000" = "App service is currently unavailable",
+    "6001" = "Insufficient privileges to access the requested app/revision",
+    "6002" = "Requested app/revision does not exist",
+    "6003" = "Requested app/revision already exists",
+    "6004" = "App name must not be null or an empty string",
+    "6006" = "Project owner must not be null or an empty string",
+    "6007" = "Project must not be null or an empty string",
+    "6008" = "App revision must not be null or an empty string",
     "6009" = "App revision must be a valid integer",
-    "6009" = "Source project must not be null or an empty string", 
-    "6010" = "Source app must not be null or an empty string.", 
+    "6009" = "Source project must not be null or an empty string",
+    "6010" = "Source app must not be null or an empty string.",
     "6011" = "Malformed app id. Expecting owner/project/app_name/revision",
 
-    ## "7xxx" = "Tasks
+    # 7xxx: Tasks
     "7000" = "Task service is currently unavailable.",
     "7001" = "Insufficient privileges to access the requested task.",
     "7002" = "Requested task does not exist",
     "7003" = "Requested task already exists",
-    "7004" = "Task id must not be empty or null or an empty string", 
-    "7005" = "Task id must be a valid UUID", 
-    "7006" = "Invalid task status. Allowed values = [QUEUED, DRAFT, RUNNING, COMPLETED, ABORTED, FAILED]", 
-    "7007" = "This action is only available for DRAFT tasks", 
-    "7008" = "This action is only available for RUNNING tasks", 
-    "7009" = "Invalid task action. Action can be performed only on DRAFT or RUNNING tasks.", 
-    "7010" = "Invalid task action. Action can be performed on DRAFT tasks", 
-    "7011" = "Invalid task action. Action can be performed on RUNNING tasks", 
-    "7012" = "Missing inputs.", 
-    "7013" = "Invalid task action.", 
-    "7014" = "Action parameter must not be null or an empty string.", 
-    "7015" = "App Id must not be null or an empty string.", 
+    "7004" = "Task id must not be empty or null or an empty string",
+    "7005" = "Task id must be a valid UUID",
+    "7006" = "Invalid task status. Allowed values = [QUEUED, DRAFT, RUNNING, COMPLETED, ABORTED, FAILED]",
+    "7007" = "This action is only available for DRAFT tasks",
+    "7008" = "This action is only available for RUNNING tasks",
+    "7009" = "Invalid task action. Action can be performed only on DRAFT or RUNNING tasks.",
+    "7010" = "Invalid task action. Action can be performed on DRAFT tasks",
+    "7011" = "Invalid task action. Action can be performed on RUNNING tasks",
+    "7012" = "Missing inputs.",
+    "7013" = "Invalid task action.",
+    "7014" = "Action parameter must not be null or an empty string.",
+    "7015" = "App Id must not be null or an empty string.",
 
-    ## "9xxx" = "General validation errors
-    "9000" = "Bad request.", 
-    "9001" = "Unauthorized.", 
-    "9002" = "Forbidden.", 
-    "9003" = "Not Found.", 
-    "9004" = "Unexpected error happened.", 
-    "9005" = "Service Unavailable.", 
-    "9006" = "Method Not Allowed.", 
-    "9007" = "Conflict.", 
-    "9008" = "Unsupported Media Type.", 
-    "9009" = "An Error occurred during the decoding of the request content.")
+    # 9xxx: General validation errors
+    "9000" = "Bad request.",
+    "9001" = "Unauthorized.",
+    "9002" = "Forbidden.",
+    "9003" = "Not Found.",
+    "9004" = "Unexpected error happened.",
+    "9005" = "Service Unavailable.",
+    "9006" = "Method Not Allowed.",
+    "9007" = "Conflict.",
+    "9008" = "Unsupported Media Type.",
+    "9009" = "An Error occurred during the decoding of the request content."
 
-
-
+)
 
 # 1. Upload files
 
@@ -266,8 +268,8 @@ upload_info = function (token = NULL, upload_id = NULL, ...) {
     if (is.null(upload_id)) stop('upload_id must be provided')
 
     req = api(token = token,
-                 path = paste0('upload/multipart/', upload_id),
-                 method = 'GET', ...)
+              path = paste0('upload/multipart/', upload_id),
+              method = 'GET', ...)
 
     return(status_check(req))
 
@@ -299,8 +301,8 @@ upload_info_part = function (token = NULL,
         stop('upload_id and part_number must be both provided')
 
     req = api(token = token,
-                 path = paste0('upload/multipart/', upload_id, '/', part_number),
-                 method = 'GET', ...)
+              path = paste0('upload/multipart/', upload_id, '/', part_number),
+              method = 'GET', ...)
 
     return(status_check(req))
 
@@ -350,7 +352,7 @@ upload_init = function (token = NULL, project_id = NULL,
     if (!is.null(part_size)) body$'part_size' = part_size
 
     req = api(token = token,
-                 path = 'upload/multipart', body = body, method = 'POST', ...)
+              path = 'upload/multipart', body = body, method = 'POST', ...)
 
     return(status_check(req))
 
@@ -388,8 +390,8 @@ upload_complete_part = function (token = NULL, upload_id = NULL,
                 'e_tag' = as.character(e_tag))
 
     req = api(token = token,
-                 path = paste0('upload/multipart/', upload_id),
-                 body = body, method = 'POST', ...)
+              path = paste0('upload/multipart/', upload_id),
+              body = body, method = 'POST', ...)
 
     return(status_check(req))
 
@@ -416,8 +418,8 @@ upload_complete_all = function (token = NULL, upload_id = NULL, ...) {
     if (is.null(upload_id)) stop('upload_id must be provided')
 
     req = api(token = token,
-                 path = paste0('upload/multipart/', upload_id, '/complete'),
-                 method = 'POST', ...)
+              path = paste0('upload/multipart/', upload_id, '/complete'),
+              method = 'POST', ...)
 
     return(status_check(req))
 
@@ -443,7 +445,7 @@ upload_delete = function (token = NULL, upload_id = NULL, ...) {
     if (is.null(upload_id)) stop('upload_id must be provided')
 
     req = api(token = token,
-                 path = paste0('upload/multipart/', upload_id), method = 'DELETE')
+              path = paste0('upload/multipart/', upload_id), method = 'DELETE')
 
     return(status_check(req))
 
@@ -470,7 +472,7 @@ project_details = function (token = NULL, project_id = NULL, ...) {
     if (is.null(project_id)) stop('project_id must be provided')
 
     req = api(token = token,
-                 path = paste0('project/', project_id), method = 'GET', ...)
+              path = paste0('project/', project_id), method = 'GET', ...)
 
     return(status_check(req))
 
@@ -498,10 +500,9 @@ project_members = function (token = NULL, project_id = NULL, ...) {
     if (is.null(project_id)) stop('project_id must be provided')
 
     req = api(token = token,
-                 path = paste0('project/', project_id, '/members'),
-                 method = 'GET', ...)
+              path = paste0('project/', project_id, '/members'),
+              method = 'GET', ...)
 
     return(status_check(req))
 
 }
-
