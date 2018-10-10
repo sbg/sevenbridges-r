42d0a0986ec79aed7ea3fcc98cd4f708dad3525c
Nan Xiao <road2stat@gmail.com>
2018-10-10 16:05:46 -0400

    format unit test code

--- tests/testthat/test-test_tools.R
+++ tests/testthat/test-test_tools.R
@@ -2,26 +2,34 @@ library("sevenbridges")
 
 # context("utils")
 
-in.df <- data.frame(id = c("number", "min", "max", "seed"),
-                    description = c("number of observation",
-                                    "lower limits of the distribution",
-                                    "upper limits of the distribution",
-                                    "seed with set.seed"),
-                    type     = c("integer", "float", "float", "float"),
-                    label    = c("number" ,"min", "max", "seed"),
-                    prefix   = c("--n", "--min", "--max", "--seed"),
-                    default  = c(1, 0, 10, 123),
-                    required = c(TRUE, FALSE, FALSE, FALSE))
-out.df <- data.frame(id   = c("res"),
-                     type = c("file"),
-                     glob = c("res.txt"))
-rbx <- Tool(id = "runif",
-            label = "Random number generator",
-            # hints = requirements(docker(pull = "ubuntu:14.04"),
-            #                      cpu(1), mem(2000)),
-            baseCommand = "echo 'radi!' > res.txt",
-            inputs      = in.df,
-            outputs     = out.df)
+in.df <- data.frame(
+  id = c("number", "min", "max", "seed"),
+  description = c(
+    "number of observation",
+    "lower limits of the distribution",
+    "upper limits of the distribution",
+    "seed with set.seed"
+  ),
+  type = c("integer", "float", "float", "float"),
+  label = c("number", "min", "max", "seed"),
+  prefix = c("--n", "--min", "--max", "--seed"),
+  default = c(1, 0, 10, 123),
+  required = c(TRUE, FALSE, FALSE, FALSE)
+)
+out.df <- data.frame(
+  id = c("res"),
+  type = c("file"),
+  glob = c("res.txt")
+)
+rbx <- Tool(
+  id = "runif",
+  label = "Random number generator",
+  # hints = requirements(docker(pull = "ubuntu:14.04"),
+  #                      cpu(1), mem(2000)),
+  baseCommand = "echo 'radi!' > res.txt",
+  inputs = in.df,
+  outputs = out.df
+)
 params <- list(number = 33)
 
 # comment this out for now to pass travis build
